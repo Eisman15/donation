505640bdff6058c2b2e521b5992b9240b864a2c3
Eisen <eismaned@yahoo.com>
2025-08-25 06:09:35 +1000

    change public IP

--- frontend/src/axiosConfig.jsx
+++ frontend/src/axiosConfig.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-//  baseURL: 'http://localhost:5001', // local
+//baseURL: 'http://localhost:5001', 
    baseURL: 'http://13.239.26.14 :5001', 
   headers: { 'Content-Type': 'application/json' },
 });
